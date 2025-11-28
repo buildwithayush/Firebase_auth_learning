1a0fbbfd1fc6858e9a82f19147e07f7e20839406
Ayush Umrao <umraoayush@gmail.com>
2025-11-28 23:31:43 +0530

    trying to setup auth with email and password

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
+   WidgetsFlutterBinding.ensureInitialized();
 
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
